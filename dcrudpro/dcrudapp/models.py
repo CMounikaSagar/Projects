accc7cf717cdf62e775da8bd5f3201351f61313b
CMounikaSagar <cmounikasagar@gmail.com>
2025-08-08 16:42:06 +0530

    deleted

--- dcrudpro/dcrudapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    s_name = models.CharField(max_length=200,null=True,unique=False)
-    s_roll_no = models.IntegerField(default=0)
-    s_address = models.CharField(max_length=200)
-    
